3f9824becafb2046919fa0e62f30c69b8fa8a259
vaniaasabila <88761105+vaniaasabila@users.noreply.github.com>
2024-06-05 17:51:08 +0700

    user home blm jadi

---
+++ src/Tab/User/UserHome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function UserHome() {
+  return (
+    <View>
+      <Text>Landing1</Text>
+    </View>
+  );
+};
